5146fee8624ddf189bd591f9cc32742bcfc932ad
MosingiA <ondicho33@gmail.com>
2025-06-08 00:17:26 +0300

    phase-1-fucntions-lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+
+const HQ_STREET = 42;
+const FEET_PER_BLOCK = 264; 
+
+function distanceFromHqInBlocks(street) {
+  return Math.abs(street - HQ_STREET);
+}
+
+function distanceFromHqInFeet(street) {
+  const blocks = distanceFromHqInBlocks(street);
+  return blocks * FEET_PER_BLOCK;
+}
+
+function distanceTravelledInFeet(startStreet, destinationStreet) {
+  const blocksTraveled = Math.abs(destinationStreet - startStreet);
+  return blocksTraveled * FEET_PER_BLOCK;
+}
+
+
+function calculatesFarePrice(startStreet, destinationStreet) {
+  const totalFeetTraveled = distanceTravelledInFeet(startStreet, destinationStreet);
+
+  if (totalFeetTraveled <= 400) {
+    return 0; 
+  } else if (totalFeetTraveled > 400 && totalFeetTraveled <= 2000) {
+    const chargeableFeet = totalFeetTraveled - 400; 
+    const priceInCents = chargeableFeet * 2; 
+    return priceInCents / 100; 
+  } else if (totalFeetTraveled > 2000 && totalFeetTraveled <= 2500) {
+    return 25; 
+  } else { 
+    return "cannot travel that far";
+  }
+}
